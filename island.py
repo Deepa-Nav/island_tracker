8b63c07176fdb4fb3121970d197d26eb8c896d69
Deepa-Nav <deepa@jstdigitaltrading.com>
2023-02-28 19:43:18 +0800

    Traversing the matrix using DFS

--- island.py
+++ island.py
@@ -1,34 +1,36 @@
-
-import os
 from typing import List
 list_of_lists = []
 class IslandTracker:
-    
+    # The traversal of the matrix is done using the Depth First Search algo. 
     with open("input_file.txt","r") as inputfile:
         for line in inputfile:
-            list_of_lists.append([line.strip()])
+            list_of_lists.append(list(line.strip()))       
+    #print(list_of_lists)
+         
+    def islands(self,matrix: List[List[str]]) -> int:
+        def search(row,col):
+            # Edge cases
+            if row<0 or col<0 or row>=rows or col >= cols or matrix[row][col] == '0' or visited[row][col]:
+                return
+            visited[row][col] = True
+            search(row+1,col) # Neighbour to right
+            search(row-1,col) # Neighbour to left
+            search(row,col+1) # Neighbour top
+            search(row,col-1) # Neighbour bottom
             
-    print(list_of_lists)
-    
-    def search(matrix,i,j):
-        pass
-    
-    def islands(matrix: List[List[str]]) -> int:
+        rows,cols = len(matrix),len(matrix[0]) 
+        visited = [[False for _ in range(cols)] for _ in range(rows)] # set initial value to 'False' as unvisited 
         numOfIslands = 0
-        rows = len(matrix)
-        cols = len(matrix[0])
-        
-        for i in range(rows):
-            for j in range(cols):
-                if matrix[i][j]=='1':
-                    IslandTracker.search(matrix,i,j)
+        for row in range(rows):
+            for col in range(cols):
+                if matrix[row][col]=='1' and not visited[row][col]: # increment the counter when its 1 and not visited
                     numOfIslands +=1
-    
-              
-            
-
+                    search(row,col)            
+        return numOfIslands
+        
 
 if __name__ == '__main__':
-       IslandTracker.islands(list_of_lists)
+    ob = IslandTracker()
+    print(ob.islands(list_of_lists))
         
         
